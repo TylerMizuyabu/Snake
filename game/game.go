a8356b79bcacff987f9340e6448d5a20ba89463d
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2021-10-30 17:23:36 -0400

    added some todos

--- game/game.go
+++ game/game.go
@@ -20,12 +20,16 @@ type Game struct {
 	apple     *Apple
 }
 
+// TODO instead of doing board size, cellsize and all this other stuff in float64 but making sure that it is not a decimal
+// should just do everything in ints. Convert all float64 into ints. Only cast into float64 when necessary
+
 func NewGame() *Game {
 	return &Game{
 		gameStart: false,
 		gameOver:  false,
 		board:     NewBoard(boardSize, boardSize),
 		snake:     NewSnake(boardSize/2, boardSize/2),
+		// TODO the apple should probably be able to do this randomization itself given a set of bounds
 		apple:     NewApple(float64(cellSize+rand.Intn(int(boardSize)-1)), float64(cellSize+rand.Intn(int(boardSize)-1))),
 	}
 }
@@ -56,6 +60,7 @@ func (g *Game) Update() error {
 		g.snake.ChangeDirection(left)
 	}
 
+	// TODO: calculations of collisions should probably be provided a struct representing the bounds of the playable area. Think about this more
 	g.gameOver = g.snake.HitWall(cellSize, cellSize) || g.snake.HitItself()
 	if g.gameOver {
 		g.snake.StopMove()
@@ -71,6 +76,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	//TODO: I don't know why when I try to draw the apple/snake on the board that it doesn't clear the past images when I redraw the board
 	g.board.Draw(screen)
 	g.apple.Draw(screen)
 	g.snake.Draw(screen)
